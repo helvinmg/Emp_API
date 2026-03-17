da0cf3acea1daf8efc1e6e9c7edc639a234ccdcd
helvinmg <helvin.m.g@gmail.com>
2026-03-17 15:39:24 +0530

    added serializers

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Employee
+from .serializers import EmployeeSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
 def emplist(request):
-    return HttpResponse("Employee List")
+    records=Employee.objects.all()#queryset
+    #serializer convert queryset to simplified data
+    serializer=EmployeeSerializer(records,many=True)
+    return Response(serializer.data)
 
 def empdetail(request,pk):
     return HttpResponse("Employee Detail for Employee id: "+str(pk))
--- empapi/settings.py
+++ empapi/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apiapp'
+    'apiapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
