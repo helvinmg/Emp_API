8095f0747230178d88ea4ea63d65672dda26876a
helvinmg <helvin.m.g@gmail.com>
2026-03-17 14:30:33 +0530

    second commit

--- apiapp/models.py
+++ apiapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name=models.CharField(max_length=50)
+    desg=models.CharField(max_length=30)
+    salary=models.FloatField()
+
+    def __str__(self):
+        return self.name+"-"+self.desg
